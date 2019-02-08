96f8c5338330af2086caacf5d8cc0ffbb3858463
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-08 16:50:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var musicians = [ "John Lennon", "Paul McCartney", "George Harrison",
 function theBeatlesPlay(musicians,instruments) {
   var newArr = []
 for (var i = 0; i < musicians.length; i++){
-  newArr.push(musicians[i] + "plays" + instruments[i])
+  newArr.push(musicians[i] + " plays " + instruments[i])
 }
 return newArr;
 }
